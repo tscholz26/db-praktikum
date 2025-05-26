7dca610817122d55e798608a0345b5dd7874a296
Tristan Scholz <tristan.scholz2000@gmail.com>
2025-05-26 12:04:15 +0200

    init springboot (add zip from spring initializer, unpack zip)

---
+++ backend/dbprak7/src/main/java/com/example/dbprak7/Dbprak7Application.java
@@ -0,0 +1,13 @@
+package com.example.dbprak7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dbprak7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dbprak7Application.class, args);
+	}
+
+}
---
+++ backend/dbprak7/src/test/java/com/example/dbprak7/Dbprak7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbprak7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dbprak7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
