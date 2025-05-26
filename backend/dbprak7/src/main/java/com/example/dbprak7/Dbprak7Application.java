362a53c12cb98a467552a146c66d2d56744f23e6
Moritz <moritzschwock@gmail.com>
2025-05-26 20:17:42 +0200

    git test

--- backend/dbprak7/src/main/java/com/example/dbprak7/Dbprak7Application.java
+++ backend/dbprak7/src/main/java/com/example/dbprak7/Dbprak7Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Dbprak7Application {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Dbprak7Application.class, args);
+		
 	}
 
 }
