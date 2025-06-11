138984cea41f5f3168b8b775ff28fdcde1adcb8e
Moritz <moritzschwock@gmail.com>
2025-06-11 22:38:44 +0200

    updated trigger to works also on delete rows of rezension

--- import/DataImportDBP7/CreateTables.sql
+++ import/DataImportDBP7/CreateTables.sql
@@ -238,14 +238,22 @@ CREATE INDEX IF NOT EXISTS idx_rezension_bewertung ON rezension (bewertung);
 
 -- CREATE TRIGGERS
 CREATE OR REPLACE FUNCTION update_produkt_rating() RETURNS TRIGGER AS $$
+DECLARE
+    produkt_id VARCHAR(50);
 BEGIN
+    IF TG_OP = 'DELETE' THEN
+        produkt_id := OLD.PNr;
+    ELSE
+        produkt_id := NEW.PNr;
+    END IF;
+
     UPDATE Produkt
     SET Rating = (
         SELECT ROUND(AVG(Bewertung)::numeric, 2)
         FROM Rezension
-        WHERE PNr = NEW.PNr
+        WHERE PNr = produkt_id
     )
-    WHERE PNr = NEW.PNr;
+    WHERE PNr = produkt_id;
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
