a2c0eacad6986c3daf9de4a07ef8f6218a712f86
Tristan Scholz <tristan.scholz2000@gmail.com>
2025-08-01 12:00:22 +0200

    - init frontend: install vue using npm, setup router

---
+++ frontend/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import InitPage from '../pages/InitPage.vue';
+import ItemOverview from '../pages/ItemOverview.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  
+  routes: [
+    {
+      path: '/',
+      name: 'initPage',
+      component: InitPage,
+    },
+    {
+      path: '/shop',
+      name: 'itemOverwiev',
+      component: ItemOverview,
+    },
+  ],
+})
+
+export default router;
