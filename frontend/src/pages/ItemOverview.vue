fdffccbed92eb711bc9051fd1cdaf72bec0cbabd
Tristan Scholz <tristan.scholz2000@gmail.com>
2025-08-04 10:19:25 +0200

    - add global css file for styling - add dummy buttons to /shop page

--- frontend/src/pages/ItemOverview.vue
+++ frontend/src/pages/ItemOverview.vue
@@ -3,9 +3,34 @@
 </script>
 
 <template>
-  <h1>STARTSEITE DES MEDIA STORES</h1>
+  <div class="container">
+    <div class="header">
+      <button>Filtern: asin</button>
+      <button>Filtern: Titel</button>
+      <button>Filtern: Kategorie</button>
+      <button>Filtern: Rating</button>
+      <button>getTrolls</button>
+      <button>Finish</button>
+    </div>
+
+    <div>
+      <h1>Auflistung der Produkte...</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .header {
+    position: fixed;
+    top: 20px;
+    left: 20px;
+    width: 100%;
+    border-bottom: 1px solid #ccc; /* Optional: visual separation */
+  }
 
 </style>
